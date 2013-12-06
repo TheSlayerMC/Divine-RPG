a66fb6457a8dacee15ec6b436eb5187e1c9dc7ba
The_SlayerMC <brock.kerley@hotmail.com>
2013-12-06 15:34:05 -0800

    Registered the ore generator again

--- net/divinerpg/helper/WorldGenHelper.java
+++ net/divinerpg/helper/WorldGenHelper.java
@@ -1,10 +1,7 @@
 package net.divinerpg.helper;
 
-import net.divinerpg.DivineRPG;
-import net.divinerpg.helper.handlers.GuiHandler;
 import net.divinerpg.overworld.gen.WorldGenOverworld;
-import net.divinerpg.worldgen.WorldGenDivineMinable;
-import cpw.mods.fml.common.network.NetworkRegistry;
+import net.divinerpg.worldgen.WorldGenDivineOres;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class WorldGenHelper {
@@ -13,7 +10,7 @@ public class WorldGenHelper {
      * Registers world generators.
      */
     public static void init(){
-        GameRegistry.registerWorldGenerator(new WorldGenDivineMinable());
+        GameRegistry.registerWorldGenerator(new WorldGenDivineOres());
         GameRegistry.registerWorldGenerator(new WorldGenOverworld());
     }
 
--- net/divinerpg/worldgen/WorldGenDivineOres.java
+++ net/divinerpg/worldgen/WorldGenDivineOres.java
@@ -13,16 +13,11 @@ import cpw.mods.fml.common.IWorldGenerator;
 public class WorldGenDivineOres implements IWorldGenerator {
 	@Override
 	public void generate(Random var1, int var2, int var3, World var4, IChunkProvider var5, IChunkProvider var6) {
-		switch(var4.provider.dimensionId) {
-		// case -1:
-		// this.generateNether(var4, var1, var2 * 16, var3 * 16);
-
+		switch (var4.provider.dimensionId) {
+		case -1:
+			this.generateNether(var4, var1, var2 * 16, var3 * 16);
 		case 0:
 			this.generateSurface(var4, var1, var2 * 16, var3 * 16);
-
-			// case 1:
-			// this.generateEnd(var4, var1, var2 * 16, var3 * 16);
-
 		default:
 		}
 	}
@@ -37,40 +32,40 @@ public class WorldGenDivineOres implements IWorldGenerator {
 		int var7;
 		int var8;
 
-		for(var5 = 0; var5 < 5; ++var5) {
+		for (var5 = 0; var5 < 6; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(20);
 			var8 = var4 + var2.nextInt(16);
 			(new WorldGenMinable(OverworldBlockHelper.realmiteOre.blockID, 5)).generate(var1, var2, var6, var7, var8);
 		}
 
-		for(var5 = 0; var5 < 3; ++var5) {
+		for (var5 = 0; var5 < 4; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(15);
 			var8 = var4 + var2.nextInt(16);
 			(new WorldGenMinable(OverworldBlockHelper.arlemiteOre.blockID, 3)).generate(var1, var2, var6, var7, var8);
 		}
 
-		for(var5 = 0; var5 < 3; ++var5) {
+		for (var5 = 0; var5 < 4; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(15);
 			var8 = var4 + var2.nextInt(16);
 			(new WorldGenMinable(OverworldBlockHelper.rupeeOre.blockID, 3)).generate(var1, var2, var6, var7, var8);
 		}
 
-		for(var5 = 0; var5 < 3; ++var5) {
+		for (var5 = 0; var5 < 3; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(16);
 			var8 = var4 + var2.nextInt(16);
 			(new WorldGenMinable(Block.oreDiamond.blockID, 6)).generate(var1, var2, var6, var7, var8);
 		}
 
-		if(var2.nextInt(8) == 0) {
+		if (var2.nextInt(8) == 0) {
 			var5 = var3 + var2.nextInt(16) + 8;
 			var6 = var2.nextInt(128);
 			var7 = var4 + var2.nextInt(16) + 8;
 
-			if(var6 < 63 || var2.nextInt(10) == 0) {
+			if (var6 < 63 || var2.nextInt(10) == 0) {
 				(new WorldGenLakes(OverworldBlockHelper.Tar.blockID)).generate(var1, var2, var5, var6, var7);
 			}
 		}
@@ -82,14 +77,14 @@ public class WorldGenDivineOres implements IWorldGenerator {
 		int var7;
 		int var8;
 
-		for(var5 = 0; var5 < 5; ++var5) {
+		for (var5 = 0; var5 < 7; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(128);
 			var8 = var4 + var2.nextInt(16);
 			(new WorldGenMinable(OverworldBlockHelper.netheriteOre.blockID, 6, Block.netherrack.blockID)).generate(var1, var2, var6, var7, var8);
 		}
 
-		for(var5 = 0; var5 < 5; ++var5) {
+		for (var5 = 0; var5 < 7; ++var5) {
 			var6 = var3 + var2.nextInt(16);
 			var7 = var2.nextInt(128);
 			var8 = var4 + var2.nextInt(16);
